b5ced44c6220267768073703a432138b8fe2e368
Thomas Watteyne <twatteyne@gmail.com>
2019-10-07 21:11:46 +0200

    DEMO-1. Fix.

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -7,7 +7,7 @@ def syscall(cmd):
 
 def test_compilation():
     result = syscall("echo compilation...")
-    assert result == None
+    assert result == 'poipoi'
     
 def test_bootload():
     result = syscall("echo bootload...")
